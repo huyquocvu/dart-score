0f1e52cc2de208b492d1515c72bc98a1302ed364
Huy Vu <huyquocvu@me.com>
2024-08-22 01:52:02 -0500

    swapping base and publicPath

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/dart-score/",
+  base: "/",
   publicPath: process.env.NODE_ENV === 'production' ? '/dart-score/' : '/',
 })
