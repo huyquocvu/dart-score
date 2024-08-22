c8d106cfbf42dfa2e106befad65a0decb441e7d3
Huy Vu <huyquocvu@me.com>
2024-08-22 01:53:13 -0500

    setting publicPath to /

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/",
-  publicPath: process.env.NODE_ENV === 'production' ? '/dart-score/' : '/',
+  base: "/dart-score",
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
 })
